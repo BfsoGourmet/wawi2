52a93a4c9f0e4802ea7410696edff5727e9ebda1
00SonGoku69 <aaron.brenner@gmx.ch>
2022-02-16 16:48:15 +0100

    added tutorial test example for api

---
+++ install.py
@@ -0,0 +1,8 @@
+import sys
+import subprocess
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask_restful'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ast'])
