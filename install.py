af555f8e614f1825cd8665f6e49844c45eff98f6
00SonGoku69 <aaron.brenner@gmx.ch>
2022-02-23 14:16:25 +0100

    install file fix

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask_restful'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
